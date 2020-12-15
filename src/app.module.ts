ac488bcfac699741a78e806e7f3397c1dd917ff4
thiaguin <jose.thiago.silva@ccc.ufcg.edu.br>
2020-12-14 22:08:02 -0300

    Starting backend configuration

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot()
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
