188cb78a649b958e48a1b62681e3b666d63c8ace
Thiaguin <jose.thiago.silva@ccc.ufcg.edu.br>
2021-03-20 02:47:54 -0300

    Improving ormconfig

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -10,4 +10,10 @@ module.exports = {
     entities: ['{dist, src}/**/*.entity{.ts,.js}'],
     migrations: ['{dist, src}/**/*.migration{.ts,.js}', '{dist, src}/**/*.seed{.ts,.js}'],
     subscribers: ['{dist, src}/**/*.subscriber{.ts,.js}'],
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 };
